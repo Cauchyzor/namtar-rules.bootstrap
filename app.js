5c72ab2f21b7281bd8dd5de2f69537c4b74144af
pierrot.cauchy <pierrot.cauchy@aifrance.com>
2022-05-17 17:55:42 +0200

    Load html content with app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 document.querySelectorAll("a[href='#']").forEach(node => {
     node.addEventListener("click", e => {
         console.log(e.target.attributes.data.value)
+        fetch(`src/${e.target.attributes.data.value}.html`)
+            .then(r => r.text())
+            .then(content => {
+                document.querySelector('main').innerHTML = content
+            })
     });
 })
 
