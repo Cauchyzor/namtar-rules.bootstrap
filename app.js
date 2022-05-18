666054e17b33cee50fae41a239e17caf40288424
Cauchyzor <cauchyzor@gmail.com>
2022-05-18 20:19:35 +0200

    EsLint format

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.querySelectorAll("a[href='#']").forEach(node => {
-    node.addEventListener("click", e => {
+    node.addEventListener('click', e => {
         console.log(e.target.attributes.data.value)
         fetch(`src/${e.target.attributes.data.value}.html`)
             .then(r => r.text())
             .then(content => {
                 document.querySelector('main').innerHTML = content
             })
-    });
+    })
 })
 
 console.log('App.js loaded !')
