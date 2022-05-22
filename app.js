9853c510a8c098c2a5f3b9a2992d3f7b6018c029
Cauchyzor <cauchyzor@gmail.com>
2022-05-22 17:04:33 +0200

    WIP

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-document.querySelectorAll("a[href='#']").forEach(node => {
-    node.addEventListener('click', e => {
-        fetch(`src/${e.target.attributes.data.value}.html`)
-            .then(r => r.text())
-            .then(content => {
-                document.querySelector('main').innerHTML = content
-            })
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll("a[href='#']").forEach(node => {
+        node.addEventListener('click', e => {
+            fetch(`src/${e.target.attributes.data.value}.html`)
+                .then(r => r.text())
+                .then(content => {
+                    document.querySelector('main').innerHTML = content
+                })
+        })
     })
+    console.log('App.js loaded !')
 })
-
-console.log('App.js loaded !')
